16eb4a379922eade5b0c700dc11d0f23060a98ee
Douglas Maciel <douglasmacielaraujo@outlook.com>
2025-03-05 14:32:50 -0300

    first commit

---
+++ src/main/java/com/project/smart_investment_simulator/SmartInvestmentSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.project.smart_investment_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInvestmentSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInvestmentSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/smart_investment_simulator/SmartInvestmentSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.smart_investment_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInvestmentSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
